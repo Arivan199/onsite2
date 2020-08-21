82bdab34fb33c8307cb8b602fce425793b13bed9
Arivan199 <63254290+Arivan199@users.noreply.github.com>
2020-08-21 12:09:22 +0530

    forget password php file

--- forget_password.php
+++ forget_password.php
@@ -13,10 +13,9 @@
 	if(isset($_POST['submit'])){
 		$mail=$_POST['email'];
 		if($mail!=""){
-			$query="SELECT count(*) as mailDet FROM users WHERE email='".$mail."';
+			$query="SELECT count(*) as mailDet FROM users WHERE email='$mail'";
 			$res=mysqli_query($dbcon,$query);
-			$arra=mysqli_fetch_array($res);
-			$count=$row['mailDet'];
+			$count=mysqli_num_rows($res);
 			if($count>0){
 			$msg = "Follow this Link to reset\nhttp://anything.com/reset.html";
 			mail($mail,"Password Reset",$msg);
